db7c4f0ac787d2cdf58fbb3f81e420136ff6e884
EPIICTHUNDERCAT <Rickys@Louis.local>
2017-06-01 01:07:01 -0700

    8

--- src/main/java/com/epiicthundercat/raft/registry/recipe/RecipeSeparator.java
+++ src/main/java/com/epiicthundercat/raft/registry/recipe/RecipeSeparator.java
@@ -1,207 +1,136 @@
 package com.epiicthundercat.raft.registry.recipe;
-
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-import com.google.common.collect.Lists;
-
+import com.google.common.collect.Maps;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+
 
 public class RecipeSeparator {
-	private static final List<RecipeSeparator> REGISTRY = Lists.<RecipeSeparator>newArrayList();
 
-	private ItemStack input;
-	private ItemStack output;
-	private ItemStack output2;
-	private ItemStack output3;
-	private ItemStack output4;
-	private ItemStack output5;
-	private double chance;
-	private double chanceTwo;
-	private double chanceThree;
-	private double chanceFour;
-	
-	
+	private static final RecipeSeparator PULVERIZING_BASE = new RecipeSeparator();
+    private final Map<ItemStack, ItemStack> pulvList = Maps.<ItemStack, ItemStack>newHashMap();
+    private final Map<ItemStack, Float> experienceList = Maps.<ItemStack, Float>newHashMap();
 
-	public RecipeSeparator(ItemStack in, ItemStack out, @Nullable ItemStack out2, double chance2, @Nullable ItemStack out3, double chance3, @Nullable ItemStack out4, double chance4, @Nullable ItemStack out5, double chance5 ) {
-		if (!isRecipeRegistered(this)) {
-			input = in;
-			output = out;
-			
-			if (out != null) {
-				output2 = out2;
-				chance = chance2;
-				output3 = out3;
-				chanceTwo = chance3;
-				output4 = out4;
-				chanceThree = chance4;
-				output5 = out5;
-				chanceFour = chance5;
-						
-				
-			}
-		}
-	}
+    public static RecipeSeparator instance()
+    {
+        return PULVERIZING_BASE;
+    }
 
-	public static List<RecipeSeparator> getRecipes() {
-		return REGISTRY;
-	}
+    public RecipeSeparator()
+    {
+  /*  	addPulverizingRecipeForBlock(Blocks.COBBLESTONE, new ItemStack(Blocks.GRAVEL), 1.0F);
+    	addPulverizingRecipeForBlock(Blocks.GRAVEL, new ItemStack(Blocks.SAND), 1.0F);
+    	addPulverizingRecipeForBlock(Blocks.STONE, new ItemStack(Blocks.GRAVEL), 1.0F);
+    	addPulverizingRecipeForBlock(Blocks.IRON_ORE, new ItemStack(RItems.IronDust, 2), 1.0F);
+    	addPulverizingRecipeForBlock(Blocks.GOLD_ORE, new ItemStack(RItems.GoldDust, 2), 1.0F);
+    	addPulverizingRecipeForBlock(Blocks.COAL_ORE, new ItemStack(RItems.CoalDust, 2), 1.0F);
+    	addPulverizingRecipeForBlock(Blocks.DIAMOND_ORE, new ItemStack(RItems.DiamondDust, 2), 1.0F);
+    	
+    	addPulverizingRecipeForItem(Items.IRON_INGOT, new ItemStack(RItems.IronDust), 1.0F);
+    	addPulverizingRecipeForItem(Items.COAL, new ItemStack(RItems.CoalDust), 1.0F);
+    	addPulverizingRecipeForItem(Items.GOLD_INGOT, new ItemStack(RItems.GoldDust), 1.0F);
+    	addPulverizingRecipeForItem(Items.DIAMOND, new ItemStack(RItems.DiamondDust), 1.0F);
 
-	public static void addRecipe(RecipeSeparator recipe) {
-		if (!isRecipeRegistered(recipe)) {
-			getRecipes().add(recipe);
-		}
-	}
+        addPulverizingRecipeForBlock(Blocks.LAPIS_ORE, new ItemStack(Items.DYE, 12, 4), 1.0F);
+        addPulverizingRecipeForBlock(Blocks.EMERALD_ORE, new ItemStack(Items.EMERALD, 2), 1.0F);
+        addPulverizingRecipeForBlock(Blocks.QUARTZ_ORE, new ItemStack(Items.QUARTZ, 4), 1.0F);
+        addPulverizingRecipeForBlock(Blocks.GLOWSTONE, new ItemStack(Items.GLOWSTONE_DUST, 4), 1.0F);
+        addPulverizingRecipeForBlock(Blocks.WOOL, new ItemStack(Items.STRING, 4), 1.0F);
+*/
+        String[] oreDictionary = OreDictionary.getOreNames();
 
-	public static void addRecipe(ItemStack in, ItemStack out, @Nullable ItemStack out2, double chance2, @Nullable ItemStack out3, double chance3, @Nullable ItemStack out4, double chance4, @Nullable ItemStack out5, double chance5 ) {
-		RecipeSeparator recipe = new RecipeSeparator(in, out, out2, chance2, out3, chance3, out4, chance4, out5, chance5);
-		if (!isRecipeRegistered(recipe)) {
-			getRecipes().add(new RecipeSeparator(in, out, out2, chance2, out3, chance3, out4, chance4, out5, chance5));
-		}
-	}
+        for(String entry : oreDictionary) {
+            if(entry.startsWith("dust")) {
+                List<ItemStack> oreList = OreDictionary.getOres(entry.replaceFirst("dust", "ore"));
+                if(!oreList.isEmpty()) {
+                    List<ItemStack> dustList = OreDictionary.getOres(entry);
+                    if(!dustList.isEmpty())
+                        addPulverizingRecipe(getOreDict(entry.replaceFirst("dust", "ore")), new ItemStack(dustList.get(0).getItem(), 2, dustList.get(0).getItemDamage()), 1.0F);
+                }
+            } else if(entry.startsWith("ingot")) {
+                List<ItemStack> dustList = OreDictionary.getOres(entry.replaceFirst("ingot", "dust"));
+                if(!dustList.isEmpty()) {
+                    addPulverizingRecipe(getOreDict(entry), dustList.get(0).copy(), 1.0F);
+                }
+            }
+        }
+    }
 
-	public static boolean isRecipeRegistered(RecipeSeparator recipe) {
-		ItemStack currentInput = recipe.getInput();
-		ItemStack currentOutput = recipe.getOutput();
-		for (RecipeSeparator recipe2 : getRecipes()) {
-			if (compareStacks(currentInput, recipe2.getInput()) && compareStacks(currentOutput, recipe2.getOutput())) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public void addPulverizingRecipeForBlock(Block input, ItemStack output, float experience)
+    {
+        this.addPulverizingRecipeForItem(Item.getItemFromBlock(input), output, experience);
+    }
 
-	public static boolean isValidInput(ItemStack stack) {
-		for (ItemStack currentStack : getInputList()) {
-			if (compareStacks(stack, currentStack)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public void addPulverizingRecipeForItem(Item input, ItemStack output, float experience)
+    {
+        this.addPulverizingRecipe(new ItemStack(input, 1, 32767), output, experience);
+    }
 
-	public static boolean isValidOutput(ItemStack stack) {
-		for (ItemStack currentStack : getOutputList()) {
-			if (compareStacks(stack, currentStack)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public void addPulverizingRecipe(ItemStack input, ItemStack output, float experience)
+    {
+        if (getPulverizingResult(input) != null) { net.minecraftforge.fml.common.FMLLog.info("Ignored pulverizing recipe with conflicting input: " + input + " = " + output); return; }
+        this.pulvList.put(input, output);
+        this.experienceList.put(output, Float.valueOf(experience));
+    }
 
-	public static boolean isValidSecondaryOutput(ItemStack stack) {
-		for (ItemStack currentStack : getSecondaryOutputList()) {
-			if (compareStacks(stack, currentStack)) {
-				return true;
-			}
-		}
-		return false;
-	}
 
-	public static RecipeSeparator getRecipeFromInput(ItemStack stack) {
-		for (RecipeSeparator recipe : getRecipes()) {
-			if (compareStacks(stack, recipe.getInput())) {
-				return recipe;
-			}
-		}
-		return null;
-	}
+    @Nullable
+    public ItemStack getPulverizingResult(ItemStack stack)
+    {
+        for (Entry<ItemStack, ItemStack> entry : this.pulvList.entrySet())
+        {
+            if (this.compareItemStacks(stack, (ItemStack)entry.getKey()))
+            {
+                return (ItemStack)entry.getValue();
+            }
+        }
 
-	public static RecipeSeparator getRecipeFromOutput(ItemStack stack) {
-		for (RecipeSeparator recipe : getRecipes()) {
-			if (compareStacks(stack, recipe.getOutput())) {
-				return recipe;
-			}
-		}
-		return null;
-	}
+        return null;
+    }
 
-	public static List<ItemStack> getInputList() {
-		List<ItemStack> list = Lists.<ItemStack>newArrayList();
-		for (RecipeSeparator recipe : getRecipes()) {
-			list.add(recipe.getInput());
-		}
-		return list;
-	}
+    private boolean compareItemStacks(ItemStack stack1, ItemStack stack2)
+    {
+        return stack2.getItem() == stack1.getItem() && (stack2.getMetadata() == 32767 || stack2.getMetadata() == stack1.getMetadata());
+    }
 
-	public static List<ItemStack> getOutputList() {
-		List<ItemStack> list = Lists.<ItemStack>newArrayList();
-		for (RecipeSeparator recipe : getRecipes()) {
-			list.add(recipe.getOutput());
-		}
-		return list;
-	}
+    public Map<ItemStack, ItemStack> getPulverizingList()
+    {
+        return this.pulvList;
+    }
 
-	public static List<ItemStack> getSecondaryOutputList() {
-		List<ItemStack> list = Lists.<ItemStack>newArrayList();
-		for (RecipeSeparator recipe : getRecipes()) {
-			list.add(recipe.getSecondOutput());
-		}
-		return list;
-	}
+    public float getPulverizingExperience(ItemStack stack)
+    {
+        float ret = stack.getItem().getSmeltingExperience(stack);
+        if (ret != -1) return ret;
 
-	public static boolean compareStacks(ItemStack stack1, ItemStack stack2) {
-		if (stack1 == null || stack2 == null) {
-			return stack1 == stack2;
-		}
-		if (stack1.getItem() != stack2.getItem()) {
-			return false;
-		}
-		if (stack1.getItemDamage() != stack2.getItemDamage()) {
-			return false;
-		}
-		if (!ItemStack.areItemStackTagsEqual(stack1, stack2)) {
-			return false;
-		}
-		return true;
-	}
+        for (Entry<ItemStack, Float> entry : this.experienceList.entrySet())
+        {
+            if (this.compareItemStacks(stack, (ItemStack)entry.getKey()))
+            {
+                return ((Float)entry.getValue()).floatValue();
+            }
+        }
 
-	
-	public ItemStack getInput() {
-		return input;
-	}
+        return 0.0F;
+    }
 
-	public ItemStack getOutput() {
-		return output;
-	}
+    public static ItemStack getOreDict(String oreIdName) {
+        List<ItemStack> ores = OreDictionary.getOres(oreIdName);
+        if (ores != null && ores.size() > 0)
+            return ores.get(0).copy();
+        return null;
+    }
 
-	public ItemStack getSecondOutput() {
-		return output2;
-	}
-
-	
-	
-	public double getChance() {
-		return chance;
-	}
-
-	
-	public boolean hasSecondOutput() {
-		return output2 != null;
-	}
-
-	
-	
-	
-	public double getSecondOutputChance() {
-		return chance;
-	}
-
-	
-	public int getInputCount() {
-		return getInput().stackSize;
-	}
-
-	
-	public int getOutputCount() {
-		return getOutput().stackSize;
-	}
-
-	
-	public int getSecondOutputCount() {
-		return hasSecondOutput() ? getSecondOutput().stackSize : -1;
-	}
 
 }
 
